7c8b828ed6c72b68346222199a8c68ae6e3726a4
baldyblus <www.happygamer390@gmail.com>
2025-03-10 10:47:26 +0530

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,22 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from app import db
 
-# User Model (For login system)
+db = SQLAlchemy()
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-# Expense Model (Stores expenses)
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category = db.Column(db.String(50), nullable=False)
     amount = db.Column(db.Float, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-
-# Create the tables
-with db.app.app_context():
-    db.create_all()
-  
